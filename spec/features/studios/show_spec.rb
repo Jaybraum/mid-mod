aef6be01b18ec10ad69b33168a4727b8bf5e50af
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-06-01 18:05:12 +0200

    creates show test for studio

---
+++ spec/features/studios/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Studio show page' do
+  before(:each) do
+    @studio = Studio.create!(name: 'Pixar', location: 'Emeryville')
+    @movie_1 = @studio.movies.create!(title: 'Toy Story', creation_year: 1996 , genre: 'Animated')
+    @movie_2 = @studio.movies.create!(title: 'Wall-E', creation_year: 2008 , genre: 'Animated')
+    @movie_3 = @studio.movies.create!(title: 'Cars', creation_year: 2006 , genre: 'Animated')
+  end
+
+  it 'shows the studios attributes' do
+    visit "/studios/#{@studio.id}"
+
+    expect(page).to have_content(@studio.name)
+    expect(page).to have_content(@studio.location)
+  end
+
+  xit 'shows the names of all the movies it has' do
+    visit "/studios/#{@studio.id}"
+
+    expect(page).to have_content(@movie_1.name)
+    expect(page).to have_content(@movie_2.name)
+    expect(page).to have_content(@movie_3.name)
+  end
+end
